3c6d59730e5c585df48039ed97a7783d8be34a96
anivmart18 <anivmart@hotmail.com>
2022-06-26 18:59:21 -0500

    Eercicios de Logica de Programacion continuacion

---
+++ RangoNumeros.js
@@ -0,0 +1,14 @@
+const aleatorio = Math.floor(Math.random() * 25) + 1;
+console.log("Numero de  aleatorio de 1 al 25:",aleatorio);
+
+
+let n1=15.265;
+let n2=45.895;
+let result=0;
+
+if (n1>n2){
+    console.log("El numero mayor es:", Math.round(n1));
+}
+else {
+    console.log("El numero menor es:", Math.round(n2));
+}
